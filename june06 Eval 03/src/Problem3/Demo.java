7a77531938031c2d68287811a8d4d62dc31098a3
Aadarsh Mukati <aadarsh11433@gmail.com>
2022-06-06 16:04:01 +0530

    eval

--- june06 Eval 03/src/Problem3/Demo.java
+++ june06 Eval 03/src/Problem3/Demo.java
@@ -6,6 +6,41 @@ public class Demo {
 	
 	static void showDetails(Month m) {
 		
+		if (m == Month.JANUARY) {
+			System.out.println("the month is : " + m);
+		}
+
+		if (m == Month.FEBRUARY) {
+			System.out.println("the month is : " + m);
+		}
+
+		if (m == Month.MARCH) {
+			System.out.println("the month is : " + m);
+
+		}if (m == Month.JANUARY) {
+			System.out.println("the month is : " + m);
+
+		}if (m == Month.JANUARY) {
+	
+			System.out.println("the month is : " + m);
+		}
+
+		if (m == Month.JANUARY) {
+			System.out.println("the month is : " + m);
+
+		}if (m == Month.JANUARY) {
+			System.out.println("the month is : " + m);
+
+		}if (m == Month.JANUARY) {
+	
+			System.out.println("the month is : " + m);
+		}if (m == Month.JANUARY) {
+			System.out.println("the month is : " + m);
+
+		}if (m == Month.JANUARY) {
+			System.out.println("the month is : " + m);
+
+		}
 	}
 
 	public static void main(String[] args) {
@@ -14,6 +49,8 @@ public class Demo {
 		System.out.println("Enter the name of month");
 		
 		String month = sc.next();
+		
+		
 		
 		Demo.showDetails(month);
 
